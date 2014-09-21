2c8f75ba70de793315788ef81c19ba2705507c90
gloei <glow@stuifmeel.org>
2014-09-21 18:39:01 +0200

    Assured that any asserts dump their message to the output window before breaking. Makes it easier to see what went wrong (especially for shader compilation errors).

--- Src/Libs/Shared/assert.h
+++ Src/Libs/Shared/assert.h
@@ -5,7 +5,7 @@
 #include "conversions.h"
 #include "exception.h"
 
-#define ASSERT_MSG(cond, msg) if (!(cond)) { if (IsDebuggerPresent()) DebugBreak(); throw Exception(std::string("At ")+std::string(__FILE__)+std::string(":")+Int2Str(__LINE__)+std::string(":\n")+(msg)); }
+#define ASSERT_MSG(cond, msg) if (!(cond)) { OutputDebugString(std::string(msg).c_str()); OutputDebugString("\n"); if (IsDebuggerPresent()) DebugBreak(); throw Exception(std::string("At ")+std::string(__FILE__)+std::string(":")+Int2Str(__LINE__)+std::string(":\n")+(msg)); }
 #define ASSERT(cond) ASSERT_MSG(cond, #cond)
 
 #else
