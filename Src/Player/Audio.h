cb2e9d924950a2b377f0430437f12f50946bcb98
Niels J. de Wit <ndewit@gmail.com>
2014-10-06 17:36:25 +0200

    Rocket files + link are in.

--- Src/Player/Audio.h
+++ Src/Player/Audio.h
@@ -4,6 +4,9 @@
 // #include <Windows.h>
 // #include <string>
 
+// Library required for Rocket.
+#pragma comment(lib, "ws2_32.lib")
+
 // 'iDevice' - valid device index or -1 for system default
 bool Audio_Create(unsigned int iDevice, HWND hWnd, const std::string &mp3Path, bool mute); 
 void Audio_Destroy();
